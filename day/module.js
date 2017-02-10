e6809c61285e271fa2d52b7f36e3108a11c20c88
jose.martinez <martinezmarquezjl@gmail.com>
2017-02-10 17:53:04 +0000

    module

--- day/module.js
+++ day/module.js
@@ -1,3 +1,22 @@
-'use sctrict';
+'use strict';
 
 var angular = require('angular');
+var DayDirective = require('./day.directive')
+
+// Module definition
+
+angular.module('DailyMenu.Day', [])
+	.constant('DailyMenu.Day.Config', {
+		partialPath: '/dist/js/day/'
+	})	
+	.config(config)
+	.factory('MenuFactory', MenuFactory)
+	.directive('day', 'DayDirective');
+
+// Config function
+
+var configInject = [];
+function config() {
+    // any config here
+}
+config.$inject = configInject;
