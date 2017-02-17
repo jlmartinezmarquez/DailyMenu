7ede355681610408eb78bef54fe61582c76319ad
jose.martinez <martinezmarquezjl@gmail.com>
2017-02-17 17:40:00 +0000

    files that were out of the previous commit

---
+++ menu/_module.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var angular = require('angular');
+
+var MenuDirective = require('./menu.directive')
+var MenuDataFactory = require('../services/menu.data.factory')
+
+// Module definition
+
+angular.module('DailyMenu.Menu', [])
+	.constant('DailyMenu.Menu.Config', {
+		partialPath: '/dist/js/menu/'
+	})	
+	.config(config)
+	.directive('menu', 'MenuDirective')
+	.factory('MenuDataFactory', MenuDataFactory);
+
+// Config function
+
+var configInject = [];
+function config() {
+    // any config here
+}
+
+config.$inject = configInject;
---
+++ menu/menu.directive.js
@@ -0,0 +1,60 @@
+'use strict'
+
+menu.$inject = 	'DailyMenu.Menu.Config'[
+
+];
+
+fucntion Menu(DailyMenuMenuConfig) {
+	var directive = {
+		restrict: 'E',
+		replace: true,
+		templateUrl: DailyMenuMenuConfig.partialPath.concat('menu.template.html'),
+		scope: {},
+		bindtoController: {
+
+		},
+		controllerAs: 'ctrl',
+		controller: MenuController
+	};
+
+	
+	MenuController.$inject = [
+		'MenuFactory'
+	];
+
+
+	function MenuController(MenuFactory) {
+		var vm = this;
+
+		//methods
+		vm.getAvailableMenusForLunch = getAvailableMenusForLunch;
+		vm.setAvailableMenusForLunch = setAvailableMenusForLunch;
+
+		return init();
+
+		/**
+		   * This is the init method
+		*/
+		function init() {
+			vm.getAvailableMenusForLunch();
+		}
+
+		function getAvailableMenusForLunch() {
+			MenuFactory
+				.getAvailableMenus('Lunch')
+				.then(vm.setAvailableMenusForLunch, onGetError);
+		}
+
+		function setAvailableMenusForLunch(availableMenus) {
+			vm.AvailableLunchMenus = availableMenus;
+		}
+
+		function onGetError(error){
+			console.error(error);
+		}		
+
+		return directive;
+	}
+}
+
+model.exports = menu;
